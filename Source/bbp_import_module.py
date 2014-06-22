7df2553a97b9caeddc881e9d90052f3bab2e95a5
Glendon Holst <glendon.holst@kaust.edu.sa>
2014-06-22 14:48:34 +0300

    this version better captures structural relationships, not just visual ones, but still a hack that doesn't properly capture the soma, or the outward growth of the skeleton

--- Source/bbp_import_module.py
+++ Source/bbp_import_module.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 import sys
-sys.path = ['/home/holst/Development/RTNeuron/Build/RTNeuron/lib',
-            '/home/holst/Development/RTNeuron/Build/install/lib',
-            '/home/holst/Development/RTNeuron/Build/BBPSDK/lib',
-            '/home/holst/Development/RTNeuron/Build/install/lib/python2.7/dist-packages'] + sys.path
+sys.path = ['/home/holstgr/Development/RTNeuron/Build/RTNeuron/lib',
+            '/home/holstgr/Development/RTNeuron/Build/install/lib',
+            '/home/holstgr/Development/RTNeuron/Build/BBPSDK/lib',
+            '/home/holstgr/Development/RTNeuron/Build/install/lib/python2.7/dist-packages'] + sys.path
 
 from bbp import *
--- Source/skeletonize.py
+++ Source/skeletonize.py
@@ -19,20 +19,60 @@ def create_morphology(skel):
     morphology = Morphology()
     soma = morphology.soma()
 
+    # TODO: fix - calculation of nonroot / root is a hack that doesn't capture loops
+    nodes = {}
+    nonroot_idxs = [segm.end for segm in skel.segments]
+    root_idxs = [nidx for nidx in range(0,len(skel.nodes)) if nidx not in nonroot_idxs]
+
+    for segm in skel.segments:
+        if segm.start in nonroot_idxs:
+            continue
+
+    	rnode_data = segm.points[0]
+        rnode_pos = (rnode_data.x,rnode_data.y,rnode_data.z)
+        rnode = soma.grow(rnode_pos[0], rnode_pos[1], rnode_pos[2], rnode_data.diameters[0], 
+                          Section_Type.DENDRITE)
+        rnode.move_point(0, Vector3f(rnode_pos[0], rnode_pos[1], rnode_pos[2]))
+        nodes[rnode_pos] = rnode
+        print('Root Node:'+segm.start)
+
     for segm in skel.segments :
         if len(segm.points) < 2 :
             continue
 
-        start, end = segm.points[0:2]
-        # Given that the soma is empty, the first segment start point is
-        # placed at 0, 0, 0. The position is corrected after adding the
-        # first section. However, we cannot correct the diamater.
-        section = soma.grow(end.x, end.y, end.z, end.diameters[0],
-                            Section_Type.DENDRITE)
-        section.move_point(0, Vector3f(start.x, start.y, start.z))
+        node_data, section_start = segm.points[0:2]
+        node_pos = (node_data.x,node_data.y,node_data.z)
+	
+        #print('Segment Start:'+segm.start+' End:'+segm.end)
 
+        if node_pos in nodes:
+            node = nodes[node_pos]
+        else: # TODO: fix hack to handle newly found roots, because root / nonroot logic is incomplete
+            node = soma.grow(node_pos[0], node_pos[1], node_pos[2], node_data.diameters[0], 
+                             Section_Type.DENDRITE)
+            node.move_point(0, Vector3f(node_pos[0], node_pos[1], node_pos[2]))
+            nodes[node_pos] = node
+            print('New Root Node:'+segm.start)
+
+        section = node.grow(section_start.x, section_start.y, section_start.z, section_start.diameters[0],
+                            Section_Type.DENDRITE)
+
+        # TODO: Sections should end at a node (unless there are loops)
         for pt in segm.points[2:] :
-            section.grow(pt.x, pt.y, pt.z, pt.diameters[0])
+            sec_pos = (pt.x, pt.y, pt.z)
+            if (sec_pos in nodes):
+                section.grow(pt.x, pt.y, pt.z, pt.diameters[0])
+                # TODO: if (sec_pos in nodes) section.grow(nodes[sec_pos]) else
+                #section.grow(nodes[sec_pos])
+            else:
+                section.grow(pt.x, pt.y, pt.z, pt.diameters[0])
+
+        node_data = segm.points[-1:][0]
+        node_pos = (node_data.x,node_data.y,node_data.z)
+        if node_pos not in nodes:
+            nodes[node_pos] = section.grow(node_pos[0], node_pos[1], node_pos[2], node_data.diameters[0], 
+                                Section_Type.DENDRITE)
+            #print('New Node:'+segm.end)
 
     return morphology
 
@@ -71,4 +111,11 @@ if __name__ == '__main__':
 
         with open(str(label), 'r') as f:
             skel = reader.parse(f)
+
         create_morphology_file(label, skel)
+
+    """
+    rtneuron-app.py
+    display_morphology_file('/home/holstgr/Development/Skeletonizer/oligodandrocyte/GeometrySurface.Smt.SptGraph.am.juan.h5')
+    display_morphology_file('/home/holstgr/Development/Skeletonizer/oligodandrocyte/GeometrySurface.Smt.SptGraph.am.h5')
+    """
